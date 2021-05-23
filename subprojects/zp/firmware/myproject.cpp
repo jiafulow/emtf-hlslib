ab59788cbe214cd59ad13a73608ccb1223562b9b
Jia Fu Low <jia.fu.low@cern.ch>
2021-05-23 15:22:02 -0500

    Add subprojects/zp codes

---
+++ subprojects/zp/firmware/myproject.cpp
@@ -0,0 +1,45 @@
+#include "myproject.h"
+
+using namespace emtf;
+
+// Top-level function implementation
+void myproject(
+    const top_in_t in0[TOP_N_IN],
+    top_out_t out[TOP_N_OUT]
+) {
+
+#pragma HLS PIPELINE II=pooling_config::target_ii
+
+#pragma HLS INTERFACE ap_vld port=in0
+#pragma HLS INTERFACE ap_vld port=out
+//#pragma HLS INTERFACE ap_ctrl_none port=return
+
+#pragma HLS ARRAY_RESHAPE variable=in0 complete dim=0
+#pragma HLS ARRAY_PARTITION variable=out complete dim=0
+
+  // Prepare input and output variables
+  zoning_out_t      zoning_0_out      [zoning_config::n_out];
+  pooling_out_t     pooling_0_out     [pooling_config::n_out];
+
+#pragma HLS ARRAY_PARTITION variable=zoning_0_out complete dim=0
+#pragma HLS ARRAY_PARTITION variable=pooling_0_out complete dim=0
+
+  // Loop over in0
+  LOOP_IN0: for (unsigned i = 0; i < TOP_N_IN; i++) {
+
+#pragma HLS UNROLL
+
+    zoning_0_out[i] = in0[i];
+  }  // end loop over in0
+
+  // Call
+  pooling_layer<m_zone_0_tag>(zoning_0_out, pooling_0_out);
+
+  // Copy to output
+  LOOP_OUT: for (unsigned i = 0; i < TOP_N_OUT; i++) {
+
+#pragma HLS UNROLL
+
+    out[i] = pooling_0_out[i];
+  }  // end loop over out
+}
---
+++ subprojects/zp/firmware/myproject.h
@@ -0,0 +1,24 @@
+#ifndef __MYPROJECT_H__
+#define __MYPROJECT_H__
+
+// Xilinx HLS
+#include "ap_int.h"
+#include "ap_fixed.h"
+//#include "hls_stream.h"
+
+// EMTF HLS
+#include "emtf_hlslib.h"
+
+// Defines
+typedef emtf::zoning_out_t top_in_t;
+typedef emtf::pooling_out_t top_out_t;
+constexpr int TOP_N_IN = emtf::zoning_config::n_out;
+constexpr int TOP_N_OUT = emtf::pooling_config::n_out;
+
+// Top-level function prototype
+void myproject(
+    const top_in_t in0[TOP_N_IN],
+    top_out_t out[TOP_N_OUT]
+);
+
+#endif  // __MYPROJECT_H__ not defined
